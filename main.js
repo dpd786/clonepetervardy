acb72332d317f55a30fede1874a1eb928ac3a35a
coderjoe <ma_uk@hotmail.com>
2017-05-22 13:01:42 +0100

    working on decoration arrow

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
+/*var $x = $(".navbar li div");
+$x.on("click", function() {
+  $x.removeClass("fColor");
+  $(this).addClass("fColor");
+});
+*/
+
 $("#navSearchLink").on('click', function() {
+  $("#menuDiv").slideUp(300, function() {
+    $(".search-dropdown-content").slideToggle(300);
+  });
+  $("#navMenuLink").removeClass("fColor");
   $(this).toggleClass("fColor");
-  $(".search-dropdown-content").slideToggle(300);
 });
 
 $("#navMenuLink").on('click', function() {
+  $(".search-dropdown-content").slideUp(300, function() {
+    $("#menuDiv").slideToggle(300);
+  });
+  $("#navSearchLink").removeClass("fColor");
   $(this).toggleClass("fColor");
-  $("#menuDiv").slideToggle(300);
 });
