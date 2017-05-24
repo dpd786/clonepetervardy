fefe92d6c8675419da7c850bf9ffbccfc1744529
coderjoe <ma_uk@hotmail.com>
2017-05-24 20:40:43 +0100

    finished toggle switch functionality

--- main.js
+++ main.js
@@ -1,23 +1,30 @@
+$(document).ready(function () {
+    "use strict";
+    $("#navSearchLink").on('click', function () {
+        $("#navMenuLink").removeClass("fColor");
+        $(".third").removeClass("arrowpointer");
+        $(this).toggleClass("fColor");
 
+        $("#menuDiv").slideUp(300, function () {
+            $(".search-dropdown-content").slideToggle(300);
+            $(".first").toggleClass("arrowpointer");
+        });
+    });
 
-$("#navSearchLink").on('click', function() {
-  $("#navMenuLink").removeClass("fColor");
-  $(".third").removeClass("arrowpointer");
-  $(this).toggleClass("fColor");
+    $("#navMenuLink").on('click', function () {
+        $("#navSearchLink").removeClass("fColor");
+        $(".first").removeClass("arrowpointer");
+        $(this).toggleClass("fColor");
 
-  $("#menuDiv").slideUp(300, function() {
-    $(".search-dropdown-content").slideToggle(300);
-    $(".first").toggleClass("arrowpointer");
-  });
-});
-
-$("#navMenuLink").on('click', function() {
-  $("#navSearchLink").removeClass("fColor");
-  $(".first").removeClass("arrowpointer");
-  $(this).toggleClass("fColor");
-
-  $(".search-dropdown-content").slideUp(300, function() {
-    $("#menuDiv").slideToggle(300);
-    $(".third").toggleClass("arrowpointer");
-  });
+        $(".search-dropdown-content").slideUp(300, function () {
+            $("#menuDiv").slideToggle(300);
+            $(".third").toggleClass("arrowpointer");
+        });
+    });
+    
+    $(".switch").on("click", function () {
+        $(".togButton").toggleClass("change");
+        $("#cashPrice").toggleClass("cashColor");
+        $("#monthlyPrice").toggleClass("monthlyColor");
+    });
 });
